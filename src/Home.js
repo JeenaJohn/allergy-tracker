52b57d260b507a9f38812efbadfdde275bd91460
Jeena John <jeena.john@gmail.com>
2020-09-04 23:37:28 -0700

    New home page layout

--- src/Home.js
+++ src/Home.js
@@ -5,51 +5,26 @@ function Home(props) {
   return (
     <div>
       {/* header nav bar and footer are in Layout.js */}
-      <header className="home-page">
-        <div className="header__text-box">
-          <h1 className="heading-primary">
-            <span className="heading-primary--main">Track My Allergy</span>
-          </h1>
 
-          <a href="/diary" className="btn btn-header header-main-button ">
-            Start Tracking
-          </a>
-          <a href="#section-about" className="btn btn-header btn-ghost">
-            Tell me more
-          </a>
-        </div>
-      </header>
-
-      <section id="section-about" className="section-about">
-        <div className="u-center-text u-margin-bottom-big">
-          <h2 className="heading-secondary bg-color-blue">
-            What is the pattern?
-          </h2>
-        </div>
-
-        <div className="row u-margin-left">
+      <section id="section-home" className="section-home">
+        <div className="row u-margin-left u-margin-top-huge u-margin-bottom-medium">
           <div className="col span-1-of-2 ">
-            <h3 className="heading-tertiary u-margin-bottom-small ">
-              Keep track of all the Allergy triggers of your kids
-            </h3>
-            <p className="paragraph u-margin-bottom-small ">
-              Knowing the allergy triggers is the most important step towards
-              preventing flareups. 'Track My Allergy' app helps you to track
-              flareups as well as the possible triggers.
-            </p>
-            <ul className="list u-margin-bottom-small u-margin-left">
-              <li> Track Symptoms.</li>
-              <li>Track food, weather, etc.</li>
-              <li>Track any activity, like soccer, swimming, painting, etc. </li>
-              <li>Get reports that help you in identifying a pattern.</li>
-            </ul>
-     
+            <div className="header__text-box ">
+              <h1 className="heading-primary ">
+                <span className="heading-primary--main">Track My Allergy</span>
+              </h1>
 
-            <p className="paragraph">
-              Add a profile for your kid and start tracking their allergy
-              triggers!! You can add upto five kid's profiles and track the
-              allergies separately for each of them.
-            </p>
+              <h3 className="heading-tertiary">
+                Keep track of all the flareups and allergy triggers of your kids
+              </h3>
+
+              <a
+                href="/diary"
+                className="btn btn-header u-margin-top-very-small "
+              >
+                Start Tracking
+              </a>
+            </div>
           </div>
           <div className="col span-1-of-2 ">
             <p className="paragraph">
@@ -60,6 +35,25 @@ function Home(props) {
               />
             </p>
           </div>
+        </div>
+        <div className="box-app-details">
+          <p className="paragraph u-margin-bottom-medium">
+            Knowing the allergy triggers is the most important step towards
+            preventing flareups. 'Track My Allergy' app helps you to track
+            flareups as well as the possible triggers.
+          </p>
+          <ul className="list u-margin-bottom-medium u-margin-left">
+            <li> Track Symptoms.</li>
+            <li>Track food, weather, etc.</li>
+            <li>Track any activity, like soccer, swimming, painting, etc. </li>
+            <li>Get reports that help you in identifying a pattern.</li>
+          </ul>
+
+          <p className="paragraph">
+            Add a profile for your kid and start tracking their allergy
+            triggers!! You can add upto five kid's profiles and track the
+            allergies separately for each of them.
+          </p>
         </div>
       </section>
     </div>
