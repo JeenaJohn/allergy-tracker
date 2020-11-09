c4784dd1250e1a0c429c1b68f59bf74444356c57
Jeena John <jeena.john@gmail.com>
2020-11-08 17:40:43 -0800

    Tests in ReportListView to check if child components are called

--- src/__tests__/ReportListView.test.js
+++ src/__tests__/ReportListView.test.js
@@ -1,53 +1,54 @@
 import React from "react";
 import { render, screen, cleanup } from "@testing-library/react";
 import ReportListView from "../ReportListView";
-
+import ReportListItem from "../ReportListItem";
+import BarChart from "../BarChart";
 
 jest.mock("../ReportListItem", () => {
-  const ReportListItem = () => <div />;
+  const ReportListItem = jest.fn(() => <div/>);
   return ReportListItem;
 });
 
 jest.mock("../BarChart", () => {
-  const BarChart = () => <div />;
+  const BarChart = jest.fn(() => <div/>);;
   return BarChart;
 });
 
 jest.mock("../firebase", () => {
-  // const data = {
-  //   "2020-09-17": {
-  //     symptoms: [
-  //       {
-  //         id: 1,
-  //         rash: true,
-  //         itchLevel: "2",
-  //         itchTime: "20:00",
-  //         notes: "Itchy",
-  //       },
-  //       {
-  //         id: 2,
-  //         rash: false,
-  //         itchLevel: "8",
-  //         itchTime: "12:00",
-  //         notes: "Itchy",
-  //       },
-  //     ],
-  //     food: {
-  //       breakfast: "Oats",
-  //       lunch: "Rice",
-  //       dinner: "Sandwich",
-  //       snacks: "Banana",
-  //     },
-  //     additionalData: {
-  //       ac: true,
-  //       nails: true,
-  //       outdoor: "playground",
-  //       notes: "Itchy",
-  //     },
-  //   },
-  // };
- /* for testing no reported data scenario*/
-  const data = {};
+  const data = {
+    "2020-09-17": {
+      symptoms: [
+        {
+          id: 1,
+          rash: true,
+          itchLevel: "2",
+          itchTime: "20:00",
+          notes: "Itchy",
+        },
+        {
+          id: 2,
+          rash: false,
+          itchLevel: "8",
+          itchTime: "12:00",
+          notes: "Itchy",
+        },
+      ],
+      food: {
+        breakfast: "Oats",
+        lunch: "Rice",
+        dinner: "Sandwich",
+        snacks: "Banana",
+      },
+      additionalData: {
+        ac: true,
+        nails: true,
+        outdoor: "playground",
+        notes: "Itchy",
+      },
+    },
+  };
+  /* for testing no reported data scenario*/
+  // const data = {};
 
   const snapshot = {
     val: () => data,
@@ -72,8 +73,15 @@ describe("ReportListView component renders properly", () => {
 
   afterEach(cleanup);
 
-  test("If there is no data reported for the month, user message is displayed", () => {
-    expect(screen.getByText("No data reported for the selected month")).toBeInTheDocument();
+  test("ReportListItem component is called", () => {
+    expect(ReportListItem).toHaveBeenCalled();
+  });
+  
+  test("BarChart component is called", () => {
+    expect(BarChart).toHaveBeenCalled();
   });
 
+  // test("If there is no data reported for the month, user message is displayed", () => {
+  //   expect(screen.getByText("No data reported for the selected month")).not.toBeInTheDocument();
+  // });
 });
